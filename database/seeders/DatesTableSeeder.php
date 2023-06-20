54e514595cf079ec709ffec807c8264c2fde4497
sakurai <sakurai_ch@yahoo.co.jp>
2023-06-20 09:07:00 +0900

    2023-06-19

--- database/seeders/DatesTableSeeder.php
+++ database/seeders/DatesTableSeeder.php
@@ -16,19 +16,22 @@ class DatesTableSeeder extends Seeder
     public function run()
     {
         $id = 1;
-        $date = new Carbon('2023-01-31');
+        $date = new Carbon('2023-06-28');
         $days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)'];
         $order = 1;
 
         Date::insert(['id' => $id++, 'date' => $date->format('n/j'),           'day' => $days[$date->dayOfWeek], 'order' => $order++]);
-        Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
-        $date->addDay();
-        $date->addDay();
-        $date->addDay();
-        $date->addDay();
         $date->addDay();
         Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
+        $date->addDay();
+        $date->addDay();
+        $date->addDay();
         Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
+        Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
+        $date->addDay();
+        Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
+        Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
+        $date->addDay();
         $date->addDay();
         Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
         Date::insert(['id' => $id++, 'date' => $date->addDay()->format('n/j'), 'day' => $days[$date->dayOfWeek], 'order' => $order++]);
